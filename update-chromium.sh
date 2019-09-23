cae100acb0e92e430f79c07f15e1c347f149af70
sungjin <sungjinlee83@gmail.com>
2019-09-23 23:54:14 +0900

    [BASH] For updating chromium browser

---
+++ update-chromium.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:canonical-chromium-builds/stage
+sudo apt update
+sudo apt install chromium-browser
+
+sudo apt install chromium-browser-l10n
+sudo apt install chromium-codecs-ffmpeg
+sudo apt install chromium-codecs-ffmpeg
